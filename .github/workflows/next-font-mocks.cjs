c38487720a93770d5f8191c4cda2685c3e9cff42
Ali <111257593+AliBenrami@users.noreply.github.com>
2026-05-13 20:37:52 -0500

    chore: tighten CI validation workflow

---
+++ .github/workflows/next-font-mocks.cjs
@@ -0,0 +1,16 @@
+const mockedCss = `
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 100 900;
+  font-display: swap;
+  src: url("mocked-inter-font.woff2") format("woff2");
+}
+`;
+
+module.exports = new Proxy(
+  {},
+  {
+    get: () => mockedCss
+  }
+);
